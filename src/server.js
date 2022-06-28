b4a49f34b037c109e66689e03fa52d94f8029dbb
josebernal12 <106898047+josebernal12@users.noreply.github.com>
2022-06-28 17:07:38 -0600

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -1,33 +1,40 @@
-require('dotenv').config()  
+//SETTINGS
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
 const puerto = process.env.PORT
-const { Server: IOServer } = require('socket.io') 
+const { Server: IOServer } = require('socket.io')
 const expressServer = app.listen(puerto, (error) => {
-    if(error) {
+    if (error) {
         console.log(error)
     } else {
         console.log(`Servidor escuchando puerto: ${puerto}`)
     }
 })
+
+
 const io = new IOServer(expressServer)
+// DECLARATION OF VARIABLES
 const messages = []
 const product = []
 
+
+//CONFIGURATION 
 app.use(express.static(path.join(__dirname, '/public')))
 
+//CONNECTIONS 
 
 io.on('connection', socket => {
     console.log('Se conecto un usuario:', socket.id)
     io.emit('server:mensaje', messages)
+    io.emit('server:producto', product)
 
+    socket.on('cliente:producto', products => {
+        product.push(products)
+        io.emit('server:producto', product)
 
-     socket.on('chat:mensaje',products =>{
-      product.push(products)
-      io.emit('server:chat', product)
-
-     })
+    })
 
     socket.on('cliente:mensaje', message => {
         messages.push(message)
